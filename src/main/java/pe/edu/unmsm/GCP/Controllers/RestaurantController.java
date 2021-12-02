0c3469f2a67b86cfd1cdfa4f4c1c0409f3d55e06
luisanthony196 <luis.anthony.chavarria@gmail.com>
2021-12-01 23:22:36 -0500

    controlador habilitado

---
+++ src/main/java/pe/edu/unmsm/GCP/Controllers/RestaurantController.java
@@ -0,0 +1,24 @@
+package pe.edu.unmsm.GCP.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST })
+public class RestaurantController {
+
+    @PostMapping("/caso1")
+    public String caso1(@RequestBody String barrio) {
+        return "hello";
+    }
+}
---
+++ src/main/java/pe/edu/unmsm/GCP/Utils/Archivo.java
@@ -0,0 +1,40 @@
+package pe.edu.unmsm.GCP.Utils;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.springframework.core.io.ClassPathResource;
+
+public class Archivo {
+  FileReader lectura;
+  BufferedReader contenido;
+
+  public Archivo() { }
+
+  public ArrayList<String> leer_archivo(String nomarchivo){
+    ArrayList<String> listado = new ArrayList<>();
+    String linea="";
+    try {
+      lectura= new FileReader(new ClassPathResource(nomarchivo).getFile());
+      contenido = new BufferedReader(lectura);
+      linea = contenido.readLine();
+      while(linea != null && linea.length() >0) {
+        listado.add(linea);
+        linea = contenido.readLine();
+      }
+      contenido.close();
+      lectura.close();
+
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return listado;
+
+  }
+
+}
