f9dbdede473e76a088d0ec8f6fbaccb4c607b7f4
luisanthony196 <luis.anthony.chavarria@gmail.com>
2021-12-01 21:28:35 -0500

    Base para el desarrollo

---
+++ src/main/java/pe/edu/unmsm/GCP/GcpApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unmsm.GCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/unmsm/GCP/GcpApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unmsm.GCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
